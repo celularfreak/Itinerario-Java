74719a7ad5693ac369ca5809c667c2cd65aa5a33
celularfreak <celularfreak@hotmail.com>
2021-09-21 18:53:46 +0200

    tercera subida

--- S1.01 Herència i Polimorfisme/src/exercici2/Vehicle.java
+++ S1.01 Herència i Polimorfisme/src/exercici2/Vehicle.java
@@ -4,11 +4,12 @@ public class Vehicle {
 
 	public static void main(String[] args) {
 		
-		System.out.println("El main crea un objeto de la clase para poder llamar al metodo iniciar que es no-estatic.");
+		System.out.println("El main crea un objeto de la clase para poder llamar al metodo iniciar que es no-estatic.\n");
 		
-		Vehicle vehicle = new Vehicle();
+		Vehicle berlina = new Vehicle();
 		
-		vehicle.iniciar();
+		System.out.println(berlina);
+		berlina.iniciar();
 		
 		parar();
 
@@ -16,11 +17,13 @@ public class Vehicle {
 	
 	public static void parar() {
 		System.out.println("\nA continuación se ejecuta el metodo parar.");
-		
+		System.out.println("\n\nParando...");
 	}
 	
 	public void iniciar() {
+	
 		System.out.println("\nSe ejecuta el metodo iniciar.");
+		System.out.println("\n\nIniciando...");
 		
 	}
 	
