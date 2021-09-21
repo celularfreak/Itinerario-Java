05840645ad223264045c6a7c8121e564e451e36c
celularfreak <celularfreak@hotmail.com>
2021-09-21 18:35:35 +0200

    segundo ejercicio listo

--- S1.01 Herència i Polimorfisme/src/exercici2/Vehicle.java
+++ S1.01 Herència i Polimorfisme/src/exercici2/Vehicle.java
@@ -3,11 +3,25 @@ package exercici2;
 public class Vehicle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("El main crea un objeto de la clase para poder llamar al metodo iniciar que es no-estatic.");
+		
+		Vehicle vehicle = new Vehicle();
+		
+		vehicle.iniciar();
+		
+		parar();
 
 	}
 	
-	static parar() {
+	public static void parar() {
+		System.out.println("\nA continuación se ejecuta el metodo parar.");
 		
 	}
+	
+	public void iniciar() {
+		System.out.println("\nSe ejecuta el metodo iniciar.");
+		
+	}
+	
 }
