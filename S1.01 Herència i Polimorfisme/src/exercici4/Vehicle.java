6c8a75d6deec9428892557747f798be53c5138e3
celularfreak <celularfreak@hotmail.com>
2021-09-22 19:09:53 +0200

    correccion

--- S1.01 Herència i Polimorfisme/src/exercici4/Vehicle.java
+++ S1.01 Herència i Polimorfisme/src/exercici4/Vehicle.java
@@ -2,7 +2,7 @@ package exercici4;
 
 public class Vehicle {
 
-	static final String marca = "Ford";
+	private static final String marca = "Ford";
 	final String model;
 	String color;
 	int potencia;
@@ -18,15 +18,16 @@ public class Vehicle {
 	}
 
 
+@SuppressWarnings("static-access")
 public static void main (String[] args) {
 	System.out.println("Hemos definido la marca del vehicle como Static final Ford y al crear el constructor no podremos cambiar el valor.");
-	System.out.println("\nEl modelo ha sido definido como final, por lo que al crear el objeto podemos introducir el valor pero despues on podra ser modificado.");
+	System.out.println("\nEl modelo ha sido definido como final, por lo que al crear el objeto podemos introducir el valor pero despues no podra ser modificado.");
 	System.out.println("\nCreamos una berlina modelo Focus y un suv modelo Kuga.");
 	
 	Vehicle berlina = new Vehicle("Focus", "Rojo", 120, 5);
 	Vehicle suv= new Vehicle("Kuga", "Gris", 140, 3);
 	
-	System.out.println("\nMarca: "+ berlina.marca+"\n"+"Model: "+berlina.model+"\n"+"Color: "+ berlina.color+"\n"+"Potencia: "+berlina.potencia+"\n"+"Numero de portes: "+berlina.numPortes);
+	System.out.println("\nMarca: "+ Vehicle.marca+"\n"+"Model: "+berlina.model+"\n"+"Color: "+ berlina.color+"\n"+"Potencia: "+berlina.potencia+"\n"+"Numero de portes: "+berlina.numPortes);
 
 	System.out.println("\nMarca: "+ suv.marca+"\n"+"Model: "+suv.model+"\n"+"Color: "+ suv.color+"\n"+"Potencia: "+suv.potencia+"\n"+"Numero de portes: "+suv.numPortes);
 	
