f85e58a592e021a522c5d6cc36d3ba99c39eb03e
celularfreak <celularfreak@hotmail.com>
2021-09-22 18:49:55 +0200

    1

---
+++ S1.02 Exceptions/src/exercici1/prueba.java
@@ -0,0 +1,13 @@
+package exercici1;
+
+public class prueba {
+	
+	public static void main(String[ ] args) {
+	    try {
+	      int[] myNumbers = {1, 2, 3};
+	      System.out.println(myNumbers[10]);
+	    } catch (Exception e) {
+	      System.out.println("Something went wrong.");
+	    }
+	  }
+}
