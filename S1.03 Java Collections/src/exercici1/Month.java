dd7e1bbef372677f3a05b6c512a734466c4e283d
celularfreak <celularfreak@hotmail.com>
2021-09-27 18:11:05 +0200

    Primer exercisi

---
+++ S1.03 Java Collections/src/exercici1/Month.java
@@ -0,0 +1,39 @@
+package exercici1;
+
+import java.util.ArrayList;
+
+public class Month {
+	
+		String name;
+		
+public Month(String name) {
+			this.name = name;
+		}
+
+	public static void main(String[] args) {
+		
+		ArrayList<Month> año = new ArrayList <Month>();
+			
+		año.add(new Month("Enero"));
+		año.add(new Month("Febrero"));
+		año.add(new Month("Marzo"));
+		año.add(new Month("Abril"));
+		año.add(new Month("Mayo"));
+		año.add(new Month("Junio"));
+		año.add(new Month("Julio"));
+		año.add(new Month("Septiembre"));
+		año.add(new Month("Octubre"));
+		año.add(new Month("Noviembre"));
+		año.add(new Month("Diciembre"));
+		
+		System.out.println("\n Comprobamos la lista de meses que hemos creado\n");
+		for (Month month: año) {
+			System.out.println("Nom del mes: "+month.name);
+		}
+		System.out.println("\n Comprobamos la lista de meses que hemos creado\n");
+		año.add(7, new Month("Agosto"));
+		for (Month month: año) {
+			System.out.println("Nom del mes: "+month.name);
+		}
+	}
+}
